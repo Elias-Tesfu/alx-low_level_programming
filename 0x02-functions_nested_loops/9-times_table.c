558fe0d1cf359323ccda956cf702131c7f61cf2f
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-15 13:16:44 -0700

    cupdae

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,19 +17,19 @@ void times_table(void)
 			{
 				_putchar(result + '0');
 			}
-			else if (k <= 9)
+			else if (result <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(result + '0');
 			}
-			else if (k >= 10)
+			else if (result >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
 			}
 		}
 		_putchar('\n');

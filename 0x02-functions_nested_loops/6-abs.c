8848915b963b7646606da370c29c5e7a98812a95
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-15 12:28:31 -0700

    seventh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - it will compute the absolute value of an ing
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	int i;
+	if (i < 0)
+		return (n * -1);
+	return (i);
+}

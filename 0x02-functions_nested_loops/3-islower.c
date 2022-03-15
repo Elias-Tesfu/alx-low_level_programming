b47d9d8db33e35757b5867a6916e94d8ee806f99
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-15 11:53:21 -0700

    for the fourth one

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	if _islower(c);
+		_putchar("1\n");
+	else
+		_putchar("0\n");
+	return (0);
+}

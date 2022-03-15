ad8285716943faffef71185c9069bedd123891ee
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-15 13:22:54 -0700

    eleven

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds 2 numbers 
+ * @i, @j: the two intergers
+ * Return: the sum
+ */
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+	_putchar(result);
+	return (0);
+}

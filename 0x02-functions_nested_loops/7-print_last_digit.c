3d05bab70f0814fa56b0ae96a5ab3f7020b732d9
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-15 12:37:59 -0700

    eighth

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a num
+ * @i: is the arg
+ * Return: the last digig
+ */
+int print_last_digit(int i)
+{
+	int r;
+
+	r = i % 10;
+	return (r);
+}

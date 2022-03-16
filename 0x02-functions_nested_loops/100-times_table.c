b81ec80f5125d08627ee063ce20793ab4c9c57e2
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-16 03:29:43 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			result = i * j;
-			if (i == 0)
+			if (j == 0)
 				_putchar(result + '0');
 			else if (result < 10 && result != 0)
 			{

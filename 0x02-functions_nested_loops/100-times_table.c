303dc104970a950ad569b434cec9c21bacb3843e
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-16 03:28:35 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,38 +10,38 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 		return;
-		for (i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			result = i * j;
+			if (i == 0)
+				_putchar(result + '0');
+			else if (result < 10 && result != 0)
 			{
-				result = i * j;
-				if (i == 0)
-					_putchar(result + '0');
-				else if (result < 10 && result != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result >= 10 && result < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result / 10 + '0');
-					_putchar(result % 10 + '0');
-				}
-				else if (result >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((result / 100) + '0');
-					_putchar(result / 10 % 10 + '0');
-					_putchar(result % 10 + '0');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
 			}
-			_putchar('\n');
+			else if (result >= 10 && result < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else if (result >= 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 100) + '0');
+				_putchar(result / 10 % 10 + '0');
+				_putchar(result % 10 + '0');
+			}
 		}
+		_putchar('\n');
+	}
 }

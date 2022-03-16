058a42f2b3dcf7d33f255c7b34578e80639284e0
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-16 00:11:17 -0700

    adding advanced part 3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long first = 1, second = 2; sum;
+
+	printf("1, 2");
+	while (sum < 20365011073)
+	{
+		sum = first + second;
+		printf(", %lu", sum);
+		first = second;
+		second = sum;
+	}
+	printf("\n");
+	return (0);
+}

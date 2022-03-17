ddae82affb79d6314ac9e4018b330a8df5524433
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-17 03:21:49 -0700

    addint for the third exercise

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the multiplication of the two numbers
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}

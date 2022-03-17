2bd27decf4880e30a3477286a185989dbbcefffd
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-17 14:11:46 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,28 +13,34 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			_putchar('F');
-			_Putchar('i');
-			_putchar('z');
-			_putchar('z');
-			_putchar('B');
-			_putchar('u');
-			_putchar('z');
-			_putchar('z');
-		else if (i % 3 == 0)
+		{
 			_putchar('F');
 			_putchar('i');
 			_putchar('z');
 			_putchar('z');
-		else if (i % 5 == 0)
 			_putchar('B');
 			_putchar('u');
 			_putchar('z');
 			_putchar('z');
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
 		else
-			printf("%d ", i);
+			_putchar('0' + i);
 	}
-	printf("\n");
+	_putchar('\n');
 
 	return (0);
 }

7e3a3f9eff9a4537d9cafc7a00ddbd188def1051
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-17 00:11:46 -0700

    adding the first exercise

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is postive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	postive_or_negative(i);
+
+	return(0);
+}

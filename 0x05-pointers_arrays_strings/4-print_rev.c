4d49465a933d4bea2ef5ac0ea00752a9d43b50cc
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-21 05:26:55 -0700

    adding

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverese
+ * @s: the string
+ * Return:
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len; i < 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}

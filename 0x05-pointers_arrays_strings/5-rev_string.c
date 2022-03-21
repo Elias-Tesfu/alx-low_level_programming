8ebdfd40e2904d7d80fa4d9d02eb984fab73fb86
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-21 14:49:58 -0700

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,13 @@
 
 void rev_string(char *s)
 {
-	int i, len;
+	int i, len, temp;
 
 	len = strlen(s);
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; i < len / 2; i++)
 	{
-		s += s[i];
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }

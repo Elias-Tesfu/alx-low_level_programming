a51dca9d1174953c98c6cdd41615eeee8bf650f7
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-21 11:02:15 -0700

    adding 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string
+ * Return: the inteege
+ */
+
+int _atoi(char *s)
+{
+	int i, num, len;
+	
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+		num = num * 10 + (s[i] - 48);
+	return (num);
+}

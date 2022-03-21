eecd6bd8e7de06f70489575ee49e828204b7860d
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-21 06:03:28 -0700

    adding 0

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ * Return:
+ */
+
+void rev_string(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len - 1; i >= 0;i--)
+	{
+		printf("%c", s[i]);
+	}
+}

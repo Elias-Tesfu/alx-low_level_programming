69fcf1cae37a317e97bbba3b9bbf459b6e0ff1af
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-23 01:35:26 -0700

    adding two

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strncpy - copies a string
+ * @src: source address of the string
+ * @dest: destination address of the string
+ * @n: the number of bytes
+ * Return: a pointer to the resulting string dest/
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	while (*src && n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}

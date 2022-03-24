28a8a575fb1fec9893558f0fbd3c74cd2227b904
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-24 06:58:04 -0700

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,20 +12,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	i = j = 0;
-	while (*(dest + i))
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		i++;
+		dest[i] = src[i];
 	}
-		while (j < n && *(src + j))
-		{
-			*(dest + i) = *(src + j);
-			i++;
-			j++;
-		}
-		if (j < n)
-			*(dest + i) = *(src + j);
-		return (dest);
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }

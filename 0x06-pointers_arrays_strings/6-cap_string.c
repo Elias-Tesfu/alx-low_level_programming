a4fc6f206fced284974cd7ade206f07fac11c9a9
Elias Tefsu <eliyastesfu@gmail.com>
2022-03-25 13:15:27 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,53 @@
+#include <stdio.h>
 #include "main.h"
+/**
+ * isLower - determines whether ascii is lowercase
+ * @c: character
+ * Return: 1 if true, 0 if false
+ */
+int isLower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - determines whether ascii is a delimiter
+ * @c: character
+ * Return: 1 if true, 0 if false
+ */
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
 
 /**
  * cap_string - capitalizes all words of a string
- * @str: the string
- * Return: the capitalized character
+ * @s: input string
+ * Return: string with capitalized words
  */
-
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i;
+	char *ptr = s;
+	int foundDelimit = 1;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*s)
 	{
-		if (i == 0)
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
 		{
-			if ((str[i] >= 97) && (str[i] <= 122))
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		else if (str[i] == ' ' || str[i] == '\n')
-		{
-			i++;
-			if (str[i] > 97 && str[i] <= 122)
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
+			*s -= 32;
+			foundDelimit = 0;
 		}
 		else
-		{
-			if (str[i] >= 65 && str[i] <= 90)
-			{
-				str[i] = str[i] + 32;
-			}
-		}
+			foundDelimit = 0;
+		s++;
 	}
-	return (str);
+	return (ptr);
 }

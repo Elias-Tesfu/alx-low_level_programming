05ab3a4408bf05a3a7f9e116897ffcbeb007e849
Elias Tesfu <eliyastesfu@gmail.com>
2022-03-30 11:44:51 -0700

    adding the main file for task 4

---
+++ 0x08-recursion/4-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	 r = _pow_recursion(1, 10);
+	 printf("%d\n", r);
+	 r = _pow_recursion(1024, 0);
+	 printf("%d\n", r);
+	 r = _pow_recursion(2, 16);
+	 printf("%d\n", r);
+	 r = 

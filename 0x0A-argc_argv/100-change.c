8f85941f6c9fcce2fb3fecdfd7bd7301c399814f
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-05 06:41:38 -0700

    adding advanced task 1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to make change
+ * @argc: the size of the array argv
+ * @argv: the array of size  argc
+ * Return: 1 if no error or 0 if error
+ */
+
+int main(int argc, char *argv[])
+{

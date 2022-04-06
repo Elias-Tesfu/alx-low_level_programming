85d8b82146ec080bbb728fc8ae500424ef0b0379
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-05 23:46:55 -0700

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,18 +9,8 @@
 
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
-	int i;
-	
-	for (i = 0; i < argc; i++)
-	{
-		putchar(*argv[0]);
-	}
-	putchar('\n');
-=======
 	(void)argc;
 
 	printf("%s\n", argv[0]);
->>>>>>> c1b88f8eb2cd1c238f0c6a7e0be3bf1efcca631f
 	return (0);
 }

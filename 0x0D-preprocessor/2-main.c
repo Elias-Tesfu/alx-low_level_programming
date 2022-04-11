f49e9d082e11660d87e07faaf174f52d052ee216
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-11 11:01:13 -0700

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file it was compiled from, followed by a new line
+ * main - printss the name of the file
  *
- * Return:
+ * Return: Always 0.
  */
 
 int main(void)

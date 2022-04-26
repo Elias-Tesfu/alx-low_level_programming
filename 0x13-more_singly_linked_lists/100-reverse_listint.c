8338bfa10b7aade620c79262e4925fdbb4f8471a
Elias Tefsu <eliyastesfu@gmail.com>
2022-04-26 14:22:05 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,7 +8,7 @@
  * Return: a pointer to the first node of the reversed list
  */
 
-listint_t *reverse_listint(listint_ **head)
+listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *new, *temp;
 

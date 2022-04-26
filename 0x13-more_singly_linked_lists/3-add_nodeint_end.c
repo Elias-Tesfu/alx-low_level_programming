976377d90f1518e6e0f3c3cf7ff6bfd03525f322
Elias Tefsu <eliyastesfu@gmail.com>
2022-04-26 13:33:24 +0300

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,7 +25,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		last = *head;
-		while (last != NULL)
+		while (last->next != NULL)
 			last = last->next;
 
 		last->next = new;

bd9309a558da6443910f9f125b56bfd659428369
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-28 03:53:50 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,11 +7,11 @@
  * Return: the converted num or 0
  */
 
-void print_binary(unsigned long int n);
+void print_binary(unsigned long int n)
 {
 	if (n > 1)
 	{
 		print_binary (n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	printf("%d", n & 1);
 }

419785754bd274bcd925072f347cb82310efc61b
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-28 03:27:39 -0700

    update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary rep of a num
+ * @n: the number to be converted
+ * Return: the converted num or 0
+ */
+
+void print_binary(unsigned long int n);
+{
+	if (n > 1)
+	{
+		print_binary (n >> 1);
+	}
+	printf("%d", n & 1);
+}

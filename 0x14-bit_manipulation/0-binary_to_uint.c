eb7dc3435e282edb8acff210d5669a0b15f5d4dd
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-28 03:04:37 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -14,11 +15,16 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
-		val <<= 1;
-		val += b[i] - '0';
-		i++;
+		if (b[i] == '0' || b[i] == '1')
+		{
+			val <<= 1;
+			val += b[i] - '0';
+			i++;
+		}
+		else
+			return (0);
 	}
 	return (val);
 }

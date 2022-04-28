9b6a8251d55fee6d339ec6508fcdcebdc3cb7756
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-28 11:48:15 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,10 +9,15 @@
 
 void print_binary(unsigned long int n)
 {
-       if (n >= 1)
+	if (n >= 1)
        {
 		print_binary(n >> 1);
-		//_putchar((n & 1) + '0');
+		_putchar((n & 1) + '0');
 	}
-       printf("%d", n & 1);
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 }

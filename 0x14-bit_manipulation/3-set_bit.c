e3a75308074882fa387e4ae9d3f7dbd2b38cf7e2
Elias Tesfu <eliyastesfu@gmail.com>
2022-04-28 11:23:13 -0700

    updaae

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,5 +9,14 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	return ((1 << index) | n);
+	unsigned long int i;
+
+	if (index > 64)
+		return (-1);
+
+	for (i = 1; index > 0; index--, i *= 2)
+		;
+	*n += i;
+
+	return (1);
 }

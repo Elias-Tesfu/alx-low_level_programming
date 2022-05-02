3be2cccf529ecf6706660ae957ae7cdba3771d15
Elias Tesfu <eliyastesfu@gmail.com>
2022-05-02 10:10:29 -0700

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: the file name
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, i, r;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		i = 0;
+		while (text_content[i])
+			i++;
+		w = write(file, text_content, i);
+		if (w != i)
+			return (-1)
+	}		
+	close(file);
+	return (1);
+}

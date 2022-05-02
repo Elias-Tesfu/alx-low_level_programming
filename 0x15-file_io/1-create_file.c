c4a6bb86e105f9c945aa4d4afd3bb871f35056e6
Elias Tesfu <eliyastesfu@gmail.com>
2022-05-02 10:18:26 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 			i++;
 		w = write(file, text_content, i);
 		if (w != i)
-			return (-1)
+			return (-1);
 	}
 	close(file);
 	return (1);

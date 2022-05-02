17b4cf94fd54783eb41a1821a2bf79230f058a74
Elias Tesfu <eliyastesfu@gmail.com>
2022-05-02 10:38:52 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, o_WRONLY | O_EXCL | O_APPEND);
+	file = open(filename, O_WRONLY | O_EXCL | O_APPEND);
 	if (file < 0)
 		return (-1);
 

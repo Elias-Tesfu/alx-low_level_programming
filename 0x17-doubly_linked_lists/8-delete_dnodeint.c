39bde76e57f14776e9be629a9a9ebaafd6cbc503
Elias Tefsu <eliyastesfu@gmail.com>
2022-06-09 21:40:14 +0300

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,3 +8,38 @@
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *temp = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	while (temp->next != NULL)
+	{
+		if (count == index)
+		{
+			temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			free(temp);
+			return (1);
+		}
+		temp = temp->next;
+		count++;
+	}
+
+	return (-1);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy

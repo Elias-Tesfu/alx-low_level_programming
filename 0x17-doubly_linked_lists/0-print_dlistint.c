ae069ebd1f50b461d4ea73d1e62d48f481dcba8d
Elias Tefsu <eliyastesfu@gmail.com>
2022-06-09 19:59:48 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,14 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	int c = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		c++;
 	}
-
-	return (count);
+	return (c);
 }
